565db4f7f46a6000236d93d3e2657d3e737966d1
ugeraud <ug.ahogla@gmail.com>
2020-01-20 21:50:11 +0100

    Ajout de la classe SimplePolicy

---
+++ src/ugeraud/SimplePolicy.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package ugeraud;
+
+/**
+ * @author gerau
+ *
+ */
+public class SimplePolicy extends AbstractPolicy{
+
+	private Behavior [] tabBehavior;
+	private int [] tabTemps;
+	private int sizeBehavior;
+	private int sizeTime;
+
+	/**
+	 * 
+	 * @param nbP
+	 */
+	public SimplePolicy(int nbP) {
+		super(nbP);
+		tabBehavior = new Behavior[nbP];
+		tabTemps = new int[nbP];
+		sizeBehavior = 0;
+		sizeTime = 0;
+	}
+	
+	/**
+	 * 
+	 * @param behavior
+	 * @param time
+	 */
+	public void add(Behavior behavior, int time) {
+		tabBehavior[sizeBehavior++] = behavior;
+		tabTemps[sizeTime++] = time;
+	}
+	
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getSizeBehavior() {
+		return sizeBehavior;
+	}
+	
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getSizePhase() {
+		return sizeTime;
+	}
+	
+	@Override
+	public Behavior phase(int index) {
+		return tabBehavior[index];
+	}
+	
+	@Override
+	public int dureePhase(int index) {
+		return tabTemps[index];
+	}
+}
